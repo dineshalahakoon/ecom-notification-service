2bdb1853bb8821fb262340f0db0bc9d7db97b949
dinesh alahakoon <146316560+dineshalahakoon@users.noreply.github.com>
2025-05-30 09:58:47 +0530

    Delete ecom-icet/src/main directory

--- ecom-icet/src/main/java/com/icet/ecom/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.icet.ecom.repository;
-
-import com.icet.ecom.entity.ProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
-}
